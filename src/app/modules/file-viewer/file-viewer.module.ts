6d11daaa608d3b8188da5fb99eb0bb0fcac20174
Devashree Hingne <devashree.hingne@mindbowser.com>
2024-08-10 13:09:22 +0530

    ,

---
+++ src/app/modules/file-viewer/file-viewer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FileViewerRoutingModule } from './file-viewer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FileViewerRoutingModule
+  ]
+})
+export class FileViewerModule { }
