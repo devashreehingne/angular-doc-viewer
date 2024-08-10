2010c80c0c3bbf23d4864e698911d29c6292a162
shweta-aswale <shweta.aswale@mindbowser.com>
2024-08-10 15:40:46 +0530

    FEat: add card view

--- src/app/file-preview/file-preview.component.ts
+++ src/app/file-preview/file-preview.component.ts
@@ -6,18 +6,47 @@ import { Component } from '@angular/core';
   styleUrl: './file-preview.component.scss'
 })
 export class FilePreviewComponent {
-  uploadedImages: string[] = [];
+  uploadedImages: { url: string; name: string }[] = [];
+  selectedImage: { url: string; name: string } | null = null;
 
   onFileChange(event: any) {
-    const files = event.target.files;
-    if (files) {
-      for (let i = 0; i < files.length; i++) {
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.uploadedImages.push(e.target.result);
-        };
-        reader.readAsDataURL(files[i]);
+    if (event.target.files && event.target.files.length) {
+      for (let file of event.target.files) {
+        if (file.type.startsWith('image/')) {
+          const reader = new FileReader();
+
+          reader.onload = (e: any) => {
+            this.uploadedImages.push({ 
+              url: e.target.result, 
+              name: file.name 
+            });
+          };
+
+          reader.readAsDataURL(file);
+        }
       }
     }
   }
+
+  openModal(image: { url: string; name: string }) {
+    this.selectedImage = image;
+  }
+
+  closeModal() {
+    this.selectedImage = null;
+  }
+  // uploadedImages: string[] = [];
+
+  // onFileChange(event: any) {
+  //   const files = event.target.files;
+  //   if (files) {
+  //     for (let i = 0; i < files.length; i++) {
+  //       const reader = new FileReader();
+  //       reader.onload = (e: any) => {
+  //         this.uploadedImages.push(e.target.result);
+  //       };
+  //       reader.readAsDataURL(files[i]);
+  //     }
+  //   }
+  // }
 }
